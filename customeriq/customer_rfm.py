9602f5d1282e5f67ab107f0c4b3403c9916abb36
andrewblock <ablock@bushelpowered.com>
2020-08-16 15:42:21 -0500

    Updated name of module

--- customeriq/customer_rfm.py
+++ customeriq/customer_rfm.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import timedelta
 
 
-class CustomerIq:
+class CustomerRFM:
 
 
     def convert_to_dataframe(self, filename, customer, date, trans_id, value):
@@ -17,7 +17,7 @@ class CustomerIq:
         period = 365
         quintiles = {}
 
-        if 'csv' or 'xls' in filename.partition('.')[2]:
+        if 'csv' in filename.partition('.')[2]:
             df = pd.read_csv(filename, parse_dates=[date])
             df = df.dropna(subset=[customer])
             orders = df.groupby([trans_id, date, customer]).agg({value: lambda x: x.sum()}).reset_index()
@@ -35,6 +35,27 @@ class CustomerIq:
             rfm.rename(columns={'DaysSinceOrder': 'Recency', F"{date}": 'Frequency'}, inplace=True)
             rfm['Monetary'] = rfm[F"{customer}"].apply(lambda x: orders[(orders[F"{customer}"] == x) & \
                             (orders[F"{date}"] >= NOW - timedelta(days=period))][F"{value}"].sum())
+
+            return rfm
+
+        if 'xls' in filename.partition('.')[2]:
+            df = pd.read_excel(filename, parse_dates=[date])
+            df = df.dropna(subset=[customer])
+            orders = df.groupby([trans_id, date, customer]).agg({value: lambda x: x.sum()}).reset_index()
+            NOW = orders[date].max() + timedelta(days=1)
+            orders['DaysSinceOrder'] = orders[date].apply(lambda x: (NOW - x).days)
+
+            aggr = {
+                # the number of days since last order (Recency)
+                'DaysSinceOrder': lambda x: x.min(),
+                # the total number of orders in the last period (Frequency)
+                F"{date}": lambda x: len([d for d in x if d >= NOW - timedelta(days=period)]),
+            }
+
+            rfm = orders.groupby(customer).agg(aggr).reset_index()
+            rfm.rename(columns={'DaysSinceOrder': 'Recency', F"{date}": 'Frequency'}, inplace=True)
+            rfm['Monetary'] = rfm[F"{customer}"].apply(lambda x: orders[(orders[F"{customer}"] == x) & \
+                                                                        (orders[F"{date}"] >= NOW - timedelta(days=period))][F"{value}"].sum())
 
             return rfm
 
